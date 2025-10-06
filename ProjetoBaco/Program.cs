a27ba01333abe50a5fcdd97389bbe634311b4df1
Alessandro <alessandro.dallastta@gmail.com>
2025-10-06 18:28:14 -0300

    adicionado: - connectio string - db connectio config

--- ProjetoBaco/Program.cs
+++ ProjetoBaco/Program.cs
@@ -1,10 +1,18 @@
+using Npgsql;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
+builder.Services.AddScoped<IDbConnection>(qualquercoisa =>
+{
+    string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+    return new NpgsqlConnection(connection);
+}
+);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
